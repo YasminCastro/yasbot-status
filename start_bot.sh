92ab38fd215e6399d7f786f0ef836afa37820aac
Yasmin <yasminsdcastro@gmail.com>
2025-11-06 09:31:27 -0300

    remove: start bot

--- start_bot.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# start_bot.sh — inicia o bot WhatsApp e mostra logs no terminal
-
-cd /home/yasbot/Desktop/yasbot || exit 1
-
-# inicia o bot (exibe os logs em tempo real)
-yarn start:prod
